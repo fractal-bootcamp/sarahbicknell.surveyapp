1d63aad663e324cc0851129107a98c6d2fe62feb
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-06-07 13:22:23 -0400

    initializes database and prisma models

---
+++ app/routes/_index.tsx
@@ -0,0 +1,55 @@
+import type { ActionFunction, MetaFunction } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import client from "~/client";
+
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+
+//server side - serves html data to client ?
+export const loader = async ()  => {
+  const surveys = client.survey.findMany();
+  return { surveys: surveys };
+}
+
+
+//this is getting the form data/survey name and updatitng the data structure, then returning (handling post requests)
+export const action: ActionFunction = async ({request }) => {
+  const data = await request.formData();
+  // getting the data from form 
+
+  // await client.survey.create({
+
+  // })
+  // create a new survey based on the form 
+
+  // getting a list of the updated survey 
+}
+
+export default function Index() {
+// this is what actually renders to the client 
+  const data = useLoaderData<typeof loader>(); 
+  return (
+
+    // create  survey
+    // get all existing surveys 
+    <div>
+
+      <Form method="post"> 
+    {/* ## specifies the content of a post request, and calls Action to actually handle the post req */}
+        <label>
+          Survey name 
+        </label>
+      </Form>
+    </div>
+
+
+
+
+  )
+}
---
+++ prisma/migrations/20240607151459_initialize_database/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Survey" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Survey_pkey" PRIMARY KEY ("id")
+);
